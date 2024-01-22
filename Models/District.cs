15ac65efb36c6358d48ae6948d5e75bac2c6e2e2
Nguyen Tuan <qtngmin@gmail.com>
2024-01-22 13:30:48 +0700

    first commit - initial files for racing app

---
+++ Models/District.cs
@@ -0,0 +1,9 @@
+namespace RunGroopWebApp.Models
+{
+    public class District
+    {
+        public int Id { get; set; }
+        public string DistrictName { get; set; }
+        public string DistrictCode { get; set; }
+    }
+}
