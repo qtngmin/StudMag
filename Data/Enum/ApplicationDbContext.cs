b9e64bd28e6112f487298b75e87f2c2c0f2d01e7
Nguyen Tuan <qtngmin@gmail.com>
2024-01-24 08:25:17 +0700

    2nd commit - add seeding data class testing...

---
+++ Data/Enum/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using StudMag.Models;
+
+namespace StudMag.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
